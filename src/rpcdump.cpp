f2745065b5961c823c365e91c22f0f05ae7e27c6
unknown <dumm_heit@xyz.de>
2014-12-23 16:45:09 +0100

    fff

--- src/rpcdump.cpp
+++ src/rpcdump.cpp
@@ -6,6 +6,7 @@
 #include "bitcoinrpc.h"
 #include "ui_interface.h"
 #include "base58.h"
+#include <fstream>
 
 #include <boost/lexical_cast.hpp>
 
@@ -36,7 +37,7 @@ Value importprivkey(const Array& params, bool fHelp)
 {
     if (fHelp || params.size() < 1 || params.size() > 3)
         throw runtime_error(
-            "importprivkey <bitcrystalxprivkey> [label] [rescan=true]\n"
+            "importprivkey <bitcrystalprivkey> [label] [rescan=true]\n"
             "Adds a private key (as returned by dumpprivkey) to your wallet.");
 
     string strSecret = params[0].get_str();
@@ -81,13 +82,13 @@ Value dumpprivkey(const Array& params, bool fHelp)
 {
     if (fHelp || params.size() != 1)
         throw runtime_error(
-            "dumpprivkey <bitcrystalxaddress>\n"
-            "Reveals the private key corresponding to <bitcrystalxaddress>.");
+            "dumpprivkey <bitcrystaladdress>\n"
+            "Reveals the private key corresponding to <bitcrystaladdress>.");
 
     string strAddress = params[0].get_str();
     CBitcoinAddress address;
     if (!address.SetString(strAddress))
-        throw JSONRPCError(RPC_INVALID_ADDRESS_OR_KEY, "Invalid BitCrystalX address");
+        throw JSONRPCError(RPC_INVALID_ADDRESS_OR_KEY, "Invalid BitCrystal address");
     CKeyID keyID;
     if (!address.GetKeyID(keyID))
         throw JSONRPCError(RPC_TYPE_ERROR, "Address does not refer to a key");
@@ -96,4 +97,289 @@ Value dumpprivkey(const Array& params, bool fHelp)
     if (!pwalletMain->GetSecret(keyID, vchSecret, fCompressed))
         throw JSONRPCError(RPC_WALLET_ERROR, "Private key for address " + strAddress + " is not known");
     return CBitcoinSecret(vchSecret, fCompressed).ToString();
+}
+
+Value myimportprivkey(const Array& params, bool fHelp)
+{
+	if (fHelp || params.size() != 2)
+        throw runtime_error(
+            "myimportprivkey <bitcrystaladdress>\n"
+            "Reveals the private key corresponding to <bitcrystaladdress>.");
+
+	CBitcoinSecret cBitcoinSecret;
+	
+	string file = params[0].get_str();
+	string password = params[1].get_str();
+	
+	ifstream file2(file, ios::in | ios::binary);
+	
+    if(!file2.is_open()){
+        throw JSONRPCError(RPC_WALLET_ERROR, "File must be close!");
+        return 0;
+    }
+
+
+    while(!file2.eof()){
+        file2.read((char *)&cBitcoinSecret, sizeof(cBitcoinSecret));
+    }
+}
+Value mydumpprivkey(const Array& params, bool fHelp)
+{
+    if (fHelp || params.size() != 2)
+        throw runtime_error(
+            "mydumpprivkey <bitcrystaladdress>\n"
+            "Reveals the private key corresponding to <bitcrystaladdress>.");
+
+      if (!pwalletMain->IsLocked())
+        pwalletMain->TopUpKeyPool();
+	string file = params[0].get_str();
+	string password = params[1].get_str();
+    // Generate a new key that is added to wallet
+    CPubKey newKey;
+    if (!pwalletMain->GetKeyFromPool(newKey, false))
+        throw JSONRPCError(RPC_WALLET_KEYPOOL_RAN_OUT, "Error: Keypool ran out, please call keypoolrefill first");
+    CKeyID keyID = newKey.GetID();
+	CBitcoinAddress address(keyID);
+    CSecret vchSecret;
+    bool fCompressed;
+	string strAdress = "Unknown";
+    if (!pwalletMain->GetSecret(keyID, vchSecret, fCompressed))
+        throw JSONRPCError(RPC_WALLET_ERROR, "Private key for address " + strAddress + " is not known");
+    CBitcoinSecret cBitcoinSecret(vchSecret, fCompressed);
+	vector<unsigned char> myPassword;
+	string addr=address.ToString();
+	int lengthAddr = addr.size();
+	int lengthPassword = password.size();
+	if (lengthPassword<lengthAddr)
+        throw JSONRPCError(RPC_WALLET_ERROR, "Private key for address " + strAddress + " is not known");
+	int length = 0;
+	char * pass = password.c_str();
+	char * addre = addr.c_str();
+	for(i = 0; i < lengthPassword; i++)
+	{
+		if(i<lengthAddr)
+			myPassword.push_back((unsigned char)&pass[i] ^ (unsigned char)&addre[i]);
+			myPassword.push_back((unsigned char)&pass[i] & (unsigned char)&addre[i]);
+			myPassword.push_back((unsigned char)&pass[i] | (unsigned char)&addre[i]);
+		else
+			myPassword.push_back((unsigned char)&pass[i]);
+	}
+	uint160 myPasswordHash = Hash160(myPassword);
+	int myPasswordHashLength = sizeof(myPasswordHash);
+	vector<unsigned char> myVec;
+	for(int i = 0; i < sizeof(int); i++)
+	{
+		myVec.push_back((unsigned char)&myPasswordHashLength[i]);
+	}
+	for(int i = 0; i < 1; i++)
+	{
+		myVec.push_back((unsigned char)10);
+		myVec.push_back((unsigned char)13);
+		myVec.push_back((unsigned char)1);
+		myVec.push_back((unsigned char)2);
+		myVec.push_back((unsigned char)3);
+		myVec.push_back((unsigned char)4);
+		myVec.push_back((unsigned char)10);
+		myVec.push_back((unsigned char)13);
+	}
+	for(int i = 0; i < myPasswordHashLength; i++)
+	{
+		myVec.push_back((unsigned char)myPasswordHash[i]);
+	}
+	for(int i = 0; i < 1; i++)
+	{
+		myVec.push_back((unsigned char)10);
+		myVec.push_back((unsigned char)13);
+		myVec.push_back((unsigned char)1);
+		myVec.push_back((unsigned char)2);
+		myVec.push_back((unsigned char)3);
+		myVec.push_back((unsigned char)4);
+		myVec.push_back((unsigned char)10);
+		myVec.push_back((unsigned char)13);
+	}
+	length = sizeof(cBitcoinSecret);
+	for(int i = 0; i < sizeof(int); i++)
+	{
+		myVec.push_back((unsigned char)&length[i]);
+	}
+	for(int i = 0; i < 1; i++)
+	{
+		myVec.push_back((unsigned char)10);
+		myVec.push_back((unsigned char)13);
+		myVec.push_back((unsigned char)1);
+		myVec.push_back((unsigned char)2);
+		myVec.push_back((unsigned char)3);
+		myVec.push_back((unsigned char)4);
+		myVec.push_back((unsigned char)10);
+		myVec.push_back((unsigned char)13);
+	}
+	for(int i = 0; i < length; i++)
+	{
+		if(i < myPasswordHashLength)
+		{
+			myVec.push_back((unsigned char*)&cBitcoinSecret[i] ^ (unsigned char*)&myPasswordHash[i]);
+		} else {
+			myVec.push_back((unsigned char*)&cBitcoinSecret[i]);
+		}
+	}
+	for(int i = 0; i < 1; i++)
+	{
+		myVec.push_back((unsigned char)10);
+		myVec.push_back((unsigned char)13);
+		myVec.push_back((unsigned char)1);
+		myVec.push_back((unsigned char)2);
+		myVec.push_back((unsigned char)3);
+		myVec.push_back((unsigned char)4);
+		myVec.push_back((unsigned char)10);
+		myVec.push_back((unsigned char)13);
+	}
+	uint160 hash = Hash160(myVec);
+	int hashLength = sizeof(hash);
+	for(int i = 0; i < sizeof(int); i++)
+	{
+		myVec.push_back((unsigned char*)&hashLength[i]);
+	}
+	for(int i = 0; i < 1; i++)
+	{
+		myVec.push_back((unsigned char)10);
+		myVec.push_back((unsigned char)13);
+		myVec.push_back((unsigned char)1);
+		myVec.push_back((unsigned char)2);
+		myVec.push_back((unsigned char)3);
+		myVec.push_back((unsigned char)4);
+		myVec.push_back((unsigned char)10);
+		myVec.push_back((unsigned char)13);
+	}
+	for(int i = 0; i < hashLength; i++)
+	{
+		myVec.push_back((unsigned char*)&hash[i]);
+	}
+	for(int i = 0; i < 1; i++)
+	{
+		myVec.push_back((unsigned char)10);
+		myVec.push_back((unsigned char)13);
+		myVec.push_back((unsigned char)1);
+		myVec.push_back((unsigned char)2);
+		myVec.push_back((unsigned char)3);
+		myVec.push_back((unsigned char)4);
+		myVec.push_back((unsigned char)10);
+		myVec.push_back((unsigned char)13);
+	}
+	length = sizeof(address);
+	for(int i = 0; i < sizeof(int); i++)
+	{
+		myVec.push_back((unsigned char*)&length[i]);
+	}
+	for(int i = 0; i < 1; i++)
+	{
+		myVec.push_back((unsigned char)10);
+		myVec.push_back((unsigned char)13);
+		myVec.push_back((unsigned char)1);
+		myVec.push_back((unsigned char)2);
+		myVec.push_back((unsigned char)3);
+		myVec.push_back((unsigned char)4);
+		myVec.push_back((unsigned char)10);
+		myVec.push_back((unsigned char)13);
+	}
+	for(int i = 0; i < length; i++)
+	{
+		if(i < myPasswordHashLength)
+			myVec.push_back((unsigned char*)&address[i] ^ (unsigned char*)&myPasswordHash[i]);
+		else
+			myVec.push_back((unsigned char*)&address[i]);
+	}
+	for(int i = 0; i < 1; i++)
+	{
+		myVec.push_back((unsigned char)10);
+		myVec.push_back((unsigned char)13);
+		myVec.push_back((unsigned char)1);
+		myVec.push_back((unsigned char)2);
+		myVec.push_back((unsigned char)3);
+		myVec.push_back((unsigned char)4);
+		myVec.push_back((unsigned char)10);
+		myVec.push_back((unsigned char)13);
+	}
+	uint160 hash = Hash160(myVec);
+	hashLength = sizeof(hash);
+	for(int i = 0; i < sizeof(int); i++)
+	{
+		myVec.push_back((unsigned char*)&hashLength[i]);
+	}
+	for(int i = 0; i < 1; i++)
+	{
+		myVec.push_back((unsigned char)10);
+		myVec.push_back((unsigned char)13);
+		myVec.push_back((unsigned char)1);
+		myVec.push_back((unsigned char)2);
+		myVec.push_back((unsigned char)3);
+		myVec.push_back((unsigned char)4);
+		myVec.push_back((unsigned char)10);
+		myVec.push_back((unsigned char)13);
+	}
+	for(int i = 0; i < hashLength; i++)
+	{
+		myVec.push_back((unsigned char*)&hash[i]);
+	}
+	for(int i = 0; i < 1; i++)
+	{
+		myVec.push_back((unsigned char)10);
+		myVec.push_back((unsigned char)13);
+		myVec.push_back((unsigned char)1);
+		myVec.push_back((unsigned char)2);
+		myVec.push_back((unsigned char)3);
+		myVec.push_back((unsigned char)4);
+		myVec.push_back((unsigned char)10);
+		myVec.push_back((unsigned char)13);
+	}
+	string ve(myVec.begin(), myVec.end());
+	Array newParams;
+	newParams.push_back(addr);
+	newParams.push_back(ve);
+	string ret = (string)signmessage(newParams, false);
+	length = ret.size();
+	char * rete = ret.c_str();
+	for(int i = 0; i < sizeof(int); i++)
+	{
+		myVec.push_back((unsigned char*)&length[i]);
+	}
+	for(int i = 0; i < 1; i++)
+	{
+		myVec.push_back((unsigned char)10);
+		myVec.push_back((unsigned char)13);
+		myVec.push_back((unsigned char)1);
+		myVec.push_back((unsigned char)2);
+		myVec.push_back((unsigned char)3);
+		myVec.push_back((unsigned char)4);
+		myVec.push_back((unsigned char)10);
+		myVec.push_back((unsigned char)13);
+	}
+	for(int i = 0; i < length; i++)
+	{
+		myVec.push_back((unsigned char*)&rete[i]);
+	}
+	for(int i = 0; i < 1; i++)
+	{
+		myVec.push_back((unsigned char)10);
+		myVec.push_back((unsigned char)13);
+		myVec.push_back((unsigned char)1);
+		myVec.push_back((unsigned char)2);
+		myVec.push_back((unsigned char)3);
+		myVec.push_back((unsigned char)4);
+		myVec.push_back((unsigned char)10);
+		myVec.push_back((unsigned char)13);
+	}
+	hashLength=sizeof(myVec)+sizeof(int);
+	for(int i = 0; i < sizeof(int); i++)
+	{
+		myVec.push_back((unsigned char*)&hashLength[i]);
+	}
+	ofstream file2(file, ios::out | ios::app | ios::binary);
+
+    if(!file2.is_open()){
+        throw JSONRPCError(RPC_WALLET_ERROR, "File must be close!");
+    } else {
+        file2.write((char*)&myVec, sizeof(myVec));
+        file2.close();
+    }
+	return address.ToString();
 }
